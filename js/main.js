26f91efb6753e67531e7a215b4d0a20763fe6224
Leocamarggo <leonardo.camarggo@outlook.com>
2022-02-18 20:23:02 -0300

    Update product desktop

--- js/main.js
+++ js/main.js
@@ -5,4 +5,20 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="./images/icon-previous.svg" class="iconSlide" alt=""/></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="./images/icon-next.svg" class="iconSlide" alt=""/></button>',
     });
+    $('.bannerItemDesk').slick({
+        infinite: false,
+        arrows: false,
+        fade: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.thumbBanner',
+    });
+    $('.thumbBanner').slick({
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.bannerItemDesk',
+        dots: false,
+        focusOnSelect: true
+    });
 });
